b2992fc99fb4f42b17fa9feae393da814e002215
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-06 18:35:29 -0300

    Mongodb connecction

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack19@cluster0-wif1w.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
