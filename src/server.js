45fa6c029b00af23c5deb0ccfce243cd4daf281b
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-06 17:12:05 -0300

    Node base API with json

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  return res.send(`Hello World. Eu sou o ${req.query.name}!`);
+  return res.json({message: `Hello World! Eu sou o ${req.query.name}!`});
 });
 
 server.listen(3333);
