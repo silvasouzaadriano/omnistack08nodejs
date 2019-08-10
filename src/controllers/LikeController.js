2f4f8503ec9f6f4ab62c27b8815a72fe4ccb05fa
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-10 13:20:24 -0300

    Match functionality

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,6 +2,8 @@ const Dev = require('../models/Dev');
 
 module.exports = {
   async store(req, res) {
+    //console.log('Teste', req.io, req.connectedUsers);
+
     const { user } = req.headers;
     const { devId } = req.params;
         
@@ -12,8 +14,18 @@ module.exports = {
       return res.status(400).json({ error: 'Dev does not exists'});
     }
 
+    // Checking Users Match
     if (targetDev.likes.includes(loggedDev._id)) {
-      console.log('DEU MATCH');
+      const loggedSocket = req.connectedUsers[user];
+      const targetSocket = req.connectedUsers[devId];
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit('match', targetDev);
+      }
+
+      if (targetSocket) {
+        req.io.to(targetSocket).emit('match', loggedDev);
+      }
     }
 
     loggedDev.likes.push(targetDev._id);
--- src/server.js
+++ src/server.js
@@ -1,16 +1,56 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
 
-const server = express();
+/*const app = express();
+const server = require('http').Server('app');
+const io  = require('socket.io')(server);*/
+
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+
+// Connection between backend and frontend
+io.on('connection', socket => {
+  
+  /*console.log('Nova conexão', socket.id);
+  
+  socket.on('hello', message => {
+    console.log(message)
+  })
+
+  setTimeout(() => {
+    socket.emit('world', {
+      message: 'Omnistack'
+    })
+  },5000)*/
+
+  const { user } = socket.handshake.query;
+  
+  //console.log(user, socket.id);
+
+  connectedUsers[user] = socket.id;
+
+});
 
 mongoose.connect('mongodb+srv://omnistack:omnistack19@cluster0-wif1w.mongodb.net/omnistack8?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+//Middleware to Controller watch the users connected t give the match
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
